7c9de0504e33c8a07c56f5d244d18869acee7a15
Jose Forcade <jforcade@g.harvard.edu>
2013-11-06 16:55:09 -0500

    repushing will -All to hopefully include files out of sync

--- controllers/c_users - Copy.php
+++
@@ -1,1 +0,0 @@
-<h1>This is the profile for <?=$user_name?></h1>
--- views/v_posts_posts.php
+++ views/v_posts_posts.php
@@ -1,11 +1,5 @@
 <?php foreach($users as $user): ?>
 
-	<?php
-		echo "<pre>";
-			print_r($user);
-		echo "<pre>";
-	?>
-		
 	<?=$user['post_id']?> <?=$user['content']?><br>
 		
 	<a href='/posts/update/<?=$user['post_id']?>'>Update</a>
